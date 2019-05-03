37593c416d5505ac7a918d74a198c3e7efad042b
Thomas Chun <tchun2055@gmail.com>
2019-05-03 15:09:44 -0700

    setup scss

--- src/components/main.js
+++ src/components/main.js
@@ -4,7 +4,7 @@ export default class Main extends React.Component {
   render() {
     return (
       <div className="app-main">
-        TOMS RESOURCE SITE - Main component
+        TOMS RESOURCE SITE - Main Component
       </div>
     )
   }
